14c8babee009e5d3c8f56c61415cde373715196e
Pallabi Nanda <pnanda2705@gmail.com>
2026-04-01 20:43:24 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './components/home'
-import Navbar from './components/navbar'
+import Home from './components/Home.jsx'
+import Navbar from './components/Navbar.jsx'
 import Login from './components/Login'
 import AddStudent from './components/AddStudent.jsx'
 import { useState } from 'react'
